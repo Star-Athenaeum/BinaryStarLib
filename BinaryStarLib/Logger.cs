2800433663724adaf1fba3ab6c74ddf9c58b37be
Connor Shearer <connor_shearer@outlook.com>
2020-04-18 14:34:13 +0100

    Make it so the Logger methods are accessed statically

--- BinaryStarLib/Logger.cs
+++ BinaryStarLib/Logger.cs
@@ -1,31 +1,32 @@
 ﻿using System;
+using System.Runtime;
+using System.Diagnostics;
+using System.Reflection;
 using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
 public sealed class Logger
 {
-    private Type LogType { get; }
-    private Thread LogThread { get; }
+    private static Logger LoggerInstance;
     private BlockingCollection<LogPackage> PackageQueue { get; } = new BlockingCollection<LogPackage>();
+    private Thread LogThread { get; }
 
     static Logger()
     {
         Console.BufferWidth = Console.WindowWidth;
     }
 
-    public Logger(Type type)
+    public Logger()
     {
-        if (type == null) throw new ArgumentNullException(nameof(type));
-        if (!type.HasMethod("Main")) throw new MissingMethodException(type.Name, "Main");
-        LogType = type;
+        LoggerInstance = this;
         LogThread = new Thread(() =>
         {
             while (true)
             {
                 LogPackage pckg = PackageQueue.Take();
                 Console.WriteLine("[" + pckg.PostTime.ToString("yyyy/MM/dd HH:mm:ss.fff")
-                    + "][" + pckg.LoggerName + "][" + pckg.Level.ToString() + "]: " + pckg.Message);
+                    + "][" + pckg.Level.ToString() + "]: " + pckg.Message);
             }
         });
         LogThread.IsBackground = true;
@@ -33,26 +34,25 @@ public sealed class Logger
         ClearBuffer();
     }
 
-    public Task Log(LogLevel level = LogLevel.Info, string msg = null)
+    public static Task Log(LogLevel level = LogLevel.Info, string msg = null)
     {
-        PackageQueue.Add(new LogPackage
+        LoggerInstance.PackageQueue.Add(new LogPackage
         {
             PostTime = DateTime.Now,
             Level = level,
-            LoggerName = LogType.Name,
             Message = msg
         });
         return Task.CompletedTask;
     }
 
-    public Task DivideBuffer()
+    public static Task DivideBuffer()
     {
         string s = string.Empty;
         for (int i = 0; i < Console.BufferWidth - 1; i++) s += "-";
         return Task.CompletedTask;
     }
 
-    public Task ClearBuffer()
+    public static Task ClearBuffer()
     {
         Console.Clear();
         return Task.CompletedTask;
@@ -71,6 +71,5 @@ internal struct LogPackage
 {
     internal DateTime PostTime { get; set; }
     internal LogLevel Level { get; set; }
-    internal string LoggerName { get; set; }
     internal string Message { get; set; }
 }
