91ccfe60e6bcedc22f693820c9caa44b439d0c11
Connor Shearer <connor_shearer@outlook.com>
2020-04-18 15:47:48 +0100

    Add IEnumerableExtensions

---
+++ BinaryStarLib/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace System
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool Contains(this IEnumerable<FileInfo> enumerable, string lookingFor)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+            if (lookingFor == null) throw new ArgumentNullException(nameof(lookingFor));
+            foreach (FileInfo item in enumerable)
+            {
+                if (item.Name == lookingFor) return true;
+            }
+            return false;
+        }
+
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> enumerable, IEnumerable<T> appending)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+            if (appending == null) throw new ArgumentNullException(nameof(appending));
+            IEnumerable<T> appended = enumerable;
+            foreach (T obj in appending) appending.Append(obj);
+            return appended.Distinct();
+        }
+    }
+}
