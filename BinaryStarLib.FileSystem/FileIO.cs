7fb147ac76609ca545be19018bdbf70871ccba9b
Connor Shearer <connor_shearer@outlook.com>
2020-04-18 16:23:39 +0100

    Get IO ready

---
+++ BinaryStarLib.FileSystem/FileIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BinaryStarLib.FileSystem
+{
+    public static class FileIO
+    {
+        public static bool Write(FileInfo info, string data)
+        {
+
+        }
+
+        public static bool Read(FileInfo info, out string data)
+        {
+
+        }
+
+        public static bool OpenStream(FileInfo info)
+        {
+
+        }
+    }
+}
---
+++ BinaryStarLib.FileSystem/FileSystemIO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BinaryStarLib.FileSystem
+{
+    public static class FileSystemIO
+    {
+        public static bool CreateFile()
+        {
+
+        }
+
+        public static bool CreateDirectory()
+        {
+
+        }
+
+        public static bool DeleteFile()
+        {
+
+        }
+
+        public static bool DeleteFolder()
+        {
+
+        }
+
+        public static bool RecycleFile()
+        {
+
+        }
+        
+        public static bool RecylceDirectory()
+        {
+
+        }
+
+        public static bool FileExists()
+        {
+
+        }
+
+        public static bool DirectoryExists()
+        {
+
+        }
+    }
+}
