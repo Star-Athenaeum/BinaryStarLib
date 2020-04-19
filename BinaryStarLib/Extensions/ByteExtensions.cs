3229e2015f713f7766b02ac9539414d2f0e6bb05
Connor Shearer <connor_shearer@outlook.com>
2020-04-19 17:13:11 +0100

    Add ByteExtensions and fix method placement errors

---
+++ BinaryStarLib/Extensions/ByteExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace System
+{
+    public static class ByteExtensions
+    {
+        public static Task<T> CastFromByteArray<T>(this byte[] data)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                ms.Write(data, 0, data.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+                try
+                {
+                    return Task.FromResult((T)bf.Deserialize(ms));
+                }
+                catch (InvalidCastException)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
--- BinaryStarLib/Extensions/ObjectExtensions.cs
+++ BinaryStarLib/Extensions/ObjectExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -8,7 +7,7 @@ namespace System
 {
     public static class ObjectExtensions
     {
-        public static Task<byte[]> CastToByteArray(object data)
+        public static Task<byte[]> CastToByteArray(this object data)
         {
             if (data == null) throw new ArgumentNullException(nameof(data));
             using (MemoryStream ms = new MemoryStream())
@@ -16,25 +15,6 @@ namespace System
                 BinaryFormatter bf = new BinaryFormatter();
                 bf.Serialize(ms, data);
                 return Task.FromResult(ms.ToArray());
-            }
-        }
-
-        public static Task<T> CastFromByteArray<T>(byte[] data)
-        {
-            if (data == null) throw new ArgumentNullException(nameof(data));
-            using (MemoryStream ms = new MemoryStream())
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                ms.Write(data, 0, data.Length);
-                ms.Seek(0, SeekOrigin.Begin);
-                try
-                {
-                    return Task.FromResult((T)bf.Deserialize(ms));
-                }
-                catch (InvalidCastException)
-                {
-                    throw;
-                }
             }
         }
     }
