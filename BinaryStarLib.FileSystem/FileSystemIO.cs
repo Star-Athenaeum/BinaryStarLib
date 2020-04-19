c391a246d59d1af3c22a5469ac474f919afb049b
Connor Shearer <connor_shearer@outlook.com>
2020-04-19 15:09:11 +0100

    Add initial methods to FileSystemIO

--- BinaryStarLib.FileSystem/FileSystemIO.cs
+++ BinaryStarLib.FileSystem/FileSystemIO.cs
@@ -4,46 +4,117 @@ using System.Threading.Tasks;
 
 namespace BSL.FileSystem
 {
-    public static class FileSystemIO
-    {
-        public static bool CreateFile()
-        {
+	public static class FileSystemIO
+	{
+		public static Task Create(FileInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			File.Create(info.FullName).Dispose();
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task Create(DirectoryInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			Directory.CreateDirectory(info.FullName);
+			return Task.CompletedTask;
+		}
 
-        public static bool CreateDirectory()
-        {
+		public static Task Delete(FileInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			File.Delete(info.FullName);
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task Delete(DirectoryInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			Directory.Delete(info.FullName);
+			return Task.CompletedTask;
+		}
 
-        public static bool DeleteFile()
-        {
+		public static Task RecycleFile(FileInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(info.FullName, 
+				Microsoft.VisualBasic.FileIO.UIOption.AllDialogs, 
+				Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task RecylceDirectory(DirectoryInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			Microsoft.VisualBasic.FileIO.FileSystem.DeleteDirectory(info.FullName,
+				Microsoft.VisualBasic.FileIO.UIOption.AllDialogs, 
+				Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
+			return Task.CompletedTask;
+		}
 
-        public static bool DeleteFolder()
-        {
+		public static Task Move(FileInfo from, FileInfo to)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			File.Move(from.FullName, to.FullName);
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task Move(DirectoryInfo from, DirectoryInfo to)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			Directory.Move(from.FullName, to.FullName);
+			return Task.CompletedTask;
+		}
 
-        public static bool RecycleFile()
-        {
+		public static Task Copy(FileInfo from, FileInfo to, bool overwrite = false)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			File.Copy(from.FullName, to.FullName, overwrite);
+			return Task.CompletedTask;
+		}
 
-        }
-        
-        public static bool RecylceDirectory()
-        {
+		public static Task Copy(DirectoryInfo from, DirectoryInfo to, bool overwrite = false)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			foreach (string newPath in Directory.GetDirectories(from.FullName, "*.*", SearchOption.AllDirectories)) Directory.CreateDirectory(newPath.Replace(from.FullName, to.FullName));
+			foreach (string newPath in Directory.GetFiles(from.FullName, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(from.FullName, to.FullName), overwrite);
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task Cut(FileInfo from, FileInfo to, bool overwrite = false)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			File.Copy(from.FullName, to.FullName, overwrite);
+			File.Delete(from.FullName);
+			return Task.CompletedTask;
+		}
 
-        public static bool FileExists()
-        {
+		public static Task Cut(DirectoryInfo from, DirectoryInfo to, bool overwrite = false)
+		{
+			if (from == null) throw new ArgumentNullException(nameof(from));
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			foreach (string newPath in Directory.GetDirectories(from.FullName, "*.*", SearchOption.AllDirectories)) Directory.CreateDirectory(newPath.Replace(from.FullName, to.FullName));
+			foreach (string newPath in Directory.GetFiles(from.FullName, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(from.FullName, to.FullName), overwrite);
+			foreach (string newPath in Directory.GetFiles(from.FullName, "*.*", SearchOption.AllDirectories)) File.Delete(newPath);
+			foreach (string newPath in Directory.GetDirectories(from.FullName, "*.*", SearchOption.AllDirectories)) Directory.Delete(newPath);
+			return Task.CompletedTask;
+		}
 
-        }
+		public static Task<bool> Exists(FileInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			return Task.FromResult(File.Exists(info.FullName));
+		}
 
-        public static bool DirectoryExists()
-        {
-
-        }
-    }
+		public static Task<bool> Exists(DirectoryInfo info)
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			return Task.FromResult(Directory.Exists(info.FullName));
+		}
+	}
 }
