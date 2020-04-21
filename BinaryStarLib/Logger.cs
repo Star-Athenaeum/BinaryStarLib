2720a3d7efb126361ef182efee1f9f098a3b76f1
Connor Shearer <connor_shearer@outlook.com>
2020-04-21 19:37:14 +0100

    Make logger fully static

--- BinaryStarLib/Logger.cs
+++ BinaryStarLib/Logger.cs
@@ -4,42 +4,35 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
-public sealed class Logger
+public static class Logger
 {
-    private static Logger LoggerInstance;
-    private BlockingCollection<LogPackage> PackageQueue { get; } = new BlockingCollection<LogPackage>();
-    private Thread LogThread { get; }
+    private static BlockingCollection<LogPackage> PackageQueue { get; } = new BlockingCollection<LogPackage>();
+    private static Thread LogThread { get; }
 
     static Logger()
     {
         Console.BufferWidth = Console.WindowWidth;
-        LoggerInstance = new Logger();
-    }
-
-    public Logger()
-    {
         LogThread = new Thread(() =>
         {
             while (true)
             {
                 LogPackage pckg = PackageQueue.Take();
-                if (pckg.PostTime != null)
-                {
-                    Console.WriteLine("[" + pckg.PostTime.ToString("yyyy/MM/dd HH:mm:ss.fff")
-                        + "][" + pckg.Level.ToString() + "]: " + pckg.Message);
-                }
-                else if (pckg.Message.All(c => c == '-')) Console.WriteLine(pckg.Message);
-                else Console.WriteLine();
+                if (pckg.ClearMode == ClearMode.None) Console.WriteLine("[" + pckg.PostTime.ToString("yyyy/MM/dd HH:mm:ss.fff") + "][" + pckg.Level.ToString() + "]: " + pckg.Message);
+                else if (pckg.ClearMode == ClearMode.ClearBuffer) Console.Clear();
+                else if (pckg.ClearMode == ClearMode.EmptyLine) Console.WriteLine();
+                else if (pckg.ClearMode == ClearMode.NoFormatting) Console.WriteLine(pckg.Message);
             }
-        });
-        LogThread.IsBackground = true;
+        })
+        {
+            IsBackground = true
+        };
         LogThread.Start();
         ClearBuffer();
     }
 
     public static Task Log(LogLevel level, object msg)
     {
-        LoggerInstance.PackageQueue.Add(new LogPackage
+        PackageQueue.Add(new LogPackage
         {
             PostTime = DateTime.Now,
             Level = level,
@@ -50,7 +43,10 @@ public sealed class Logger
 
     public static Task NewLine()
     {
-        LoggerInstance.PackageQueue.Add(new LogPackage());
+        PackageQueue.Add(new LogPackage
+        {
+            ClearMode = ClearMode.EmptyLine
+        });
         return Task.CompletedTask;
     }
 
@@ -58,8 +54,9 @@ public sealed class Logger
     {
         string s = string.Empty;
         for (int i = 0; i < Console.BufferWidth - 1; i++) s += "-";
-        LoggerInstance.PackageQueue.Add(new LogPackage
+        PackageQueue.Add(new LogPackage
         {
+            ClearMode = ClearMode.NoFormatting,
             Message = s
         });
         return Task.CompletedTask;
@@ -67,7 +64,10 @@ public sealed class Logger
 
     public static Task ClearBuffer()
     {
-        Console.Clear();
+        PackageQueue.Add(new LogPackage
+        {
+            ClearMode = ClearMode.ClearBuffer
+        });
         return Task.CompletedTask;
     }
 }
@@ -80,9 +80,18 @@ public enum LogLevel
     Debug = 0b_1111_1111
 }
 
+public enum ClearMode
+{
+    None = 0b_0000_0000,
+    NoFormatting = 0b_0000_0001,
+    EmptyLine = 0b_0000_0010,
+    ClearBuffer = 0b_000_0011
+}
+
 internal struct LogPackage
 {
     internal DateTime PostTime { get; set; }
+    internal ClearMode ClearMode { get; set; }
     internal LogLevel Level { get; set; }
     internal string Message { get; set; }
 }
