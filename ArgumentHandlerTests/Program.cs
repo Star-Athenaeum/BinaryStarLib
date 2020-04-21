7df36b5857799afea6856c929162c237b23f67ff
Connor Shearer <connor_shearer@outlook.com>
2020-04-21 17:59:52 +0100

    Add ArgumentHandlerTests project

---
+++ ArgumentHandlerTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BSL;
+
+namespace ArgumentHandlerTests
+{
+    class Program
+    {
+        static void Main() => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            await ArgumentHandler.ParseArgumentsFromArray(new string[]
+            {
+                "-projectPath",
+                "D:/Projects",
+                "-deleteEverything",
+                "true"
+            }, 
+            out List<ParsedArgument> parsedArgs);
+
+            foreach (ParsedArgument arg in parsedArgs) Console.WriteLine(arg.Command + " : " + arg.Value);
+            Console.ReadKey();
+        }
+    }
+}
