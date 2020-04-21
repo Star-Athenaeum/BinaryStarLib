3ff8530d574ce0a47429c6d8c5ce868ce9cb115b
Connor Shearer <connor_shearer@outlook.com>
2020-04-21 19:15:35 +0100

    Add LoggerTests project

---
+++ LoggerTests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoggerTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.ClearBuffer();
+            Logger.Log(LogLevel.Info, 1);
+            Logger.Log(LogLevel.Warn, 455534456.CastToByteArray());
+            Logger.Log(LogLevel.Error, "Houston, we have a problem.");
+            Logger.DivideBuffer();
+            Logger.Log(LogLevel.Debug, new List<string>());
+            Console.ReadKey();
+        }
+    }
+}
