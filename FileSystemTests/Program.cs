805ea6cb5bb533e6835562398538d4829951e65f
Connor Shearer <connor_shearer@outlook.com>
2020-04-24 18:29:45 +0100

    Add FileSystemTests

---
+++ FileSystemTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using BSL;
+using BSL.FileSystem;
+
+namespace FileSystemTests
+{
+    class Program
+    {
+        static void Main() => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            await Logger.Log(LogLevel.Debug, 
+                await FileIO.ReadText(FileSystemIO.ApplicationDirectory.CombineToFile("TestingReadFile.txt"), FileMode.Open, FileAccess.Read, FileShare.Read));
+            Console.ReadKey();
+        }
+    }
+}
