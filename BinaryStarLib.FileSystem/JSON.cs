1f871cba3a2482497673fb759724a52305eeeeec
Connor Shearer <connor_shearer@outlook.com>
2020-05-07 13:20:58 +0100

    Add JSON FileIO

---
+++ BinaryStarLib.FileSystem/JSON.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace BSL.FileSystem
+{
+	public static class JSON
+	{
+		public static async Task WriteJSONSync<T>(T data, FileInfo info)
+		{
+			if (data == null) throw new ArgumentNullException(nameof(data));
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			await FileIO.WriteText(info, JsonConvert.SerializeObject(data), Encoding.UTF8);
+		}
+
+		public static async Task<T> ReadJSONSync<T>(FileInfo info) where T : new()
+		{
+			if (info == null) throw new ArgumentNullException(nameof(info));
+			return (T)JsonConvert.DeserializeObject(await FileIO.ReadText(info));
+		}
+	}
+}
