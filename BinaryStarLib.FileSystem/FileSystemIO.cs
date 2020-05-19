aa92a491ce64fa555989996c869cca36508a1bd7
Connor Shearer <connor_shearer@outlook.com>
2020-05-19 02:34:11 +0100

    Recycling functionality isnt available in .NET Standard 2.1

--- BinaryStarLib.FileSystem/FileSystemIO.cs
+++ BinaryStarLib.FileSystem/FileSystemIO.cs
@@ -49,7 +49,7 @@ namespace BSL.FileSystem
 			Directory.Delete(info.FullName);
 			return Task.CompletedTask;
 		}
-
+		/*
 		public static Task RecycleFile(FileInfo info)
 		{
 			if (info == null) throw new ArgumentNullException(nameof(info));
@@ -66,7 +66,7 @@ namespace BSL.FileSystem
 				Microsoft.VisualBasic.FileIO.UIOption.AllDialogs, 
 				Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin);
 			return Task.CompletedTask;
-		}
+		}*/
 
 		public static Task Move(FileInfo from, FileInfo to)
 		{
