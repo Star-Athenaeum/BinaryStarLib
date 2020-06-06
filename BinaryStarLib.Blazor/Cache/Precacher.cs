824dbaddcab810f9e08f33283d4b9da31ff412cb
Connor Shearer <connor_shearer@outlook.com>
2020-06-07 00:51:50 +0100

    Upgrade everything to .NET 5

---
+++ BinaryStarLib.Blazor/Cache/Precacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSL.Blazor.Cache
+{
+    public class Precacher// : IAsyncDisposable
+    {
+        //public Dictionary<string, List<byte>>
+
+        /*public ValueTask DisposeAsync()
+        {
+            return default;
+        }*/
+    }
+}
