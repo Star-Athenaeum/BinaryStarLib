cb22b566e36910f63cb00442f55b5be8c944dab3
Connor Shearer <connor_shearer@outlook.com>
2020-07-02 08:00:24 +0100

    Add runtime information store class

---
+++ BinaryStarLib/Runtime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BSL
+{
+    public static class Runtime
+    {
+        static Runtime()
+        {
+
+        }
+
+        public static bool IsMono { get { return RuntimeInformation.FrameworkDescription.Contains("Mono"); } }
+        public static bool IsWASM { get { return RuntimeInformation.FrameworkDescription.Contains("wasm"); } }
+    }
+}
