a9e2f5cb80a49ffbdc4ed0095d8b8a96b6309556
Connor Shearer <connor_shearer@outlook.com>
2020-07-02 05:56:43 +0100

    Improve LoggerTests

--- LoggerTests/Program.cs
+++ LoggerTests/Program.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LoggerTests
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        internal static void Main()
         {
             Logger.ClearBuffer();
+            Logger.LogDebug("SYNC LOGGER");
+            Logger.DivideBuffer();
             Logger.LogInfo(1);
             Logger.LogWarn(455534456.CastToByteArray());
             Logger.LogError("Houston, we dont have a problem.");
-            Logger.DivideBuffer();
             Logger.LogDebug(new List<string>());
+            Logger.NewLine(3);
+            MainAsync().GetAwaiter().GetResult();
             Console.ReadKey();
+        }
+
+        private static async Task MainAsync()
+        {
+            await Logger.LogDebug("ASYNC LOGGER");
+            await Logger.DivideBuffer();
+            await Logger.LogInfo(1);
+            await Logger.LogWarn(455534456.CastToByteArray());
+            await Logger.LogError("Houston, we dont have a problem.");
+            await Logger.LogDebug(new List<string>());
+            await Logger.DivideBuffer();
         }
     }
 }
