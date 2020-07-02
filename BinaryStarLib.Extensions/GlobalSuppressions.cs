5bee91737c986b44179642c4ced5947a5655e620
Connor Shearer <connor_shearer@outlook.com>
2020-07-02 08:00:02 +0100

    Move extensions into their own project

---
+++ BinaryStarLib.Extensions/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsDirectory(System.IO.DirectoryInfo,System.String)~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsAnyDirectory(System.IO.DirectoryInfo,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsAllDirectories(System.IO.DirectoryInfo,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsAllDirectories(System.IO.FileInfo,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsDirectory(System.IO.FileInfo,System.String)~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsAnyDirectory(System.IO.FileInfo,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.ContainsAny(System.String,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.ContainsAll(System.String,System.String[])~System.Boolean")]
+[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.GetLocationsOfString(System.String,System.String)~System.Collections.Generic.List{System.Int32}")]
--- BinaryStarLib/GlobalSuppressions.cs
+++ BinaryStarLib/GlobalSuppressions.cs
@@ -6,12 +6,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline", Justification = "<Pending>", Scope = "member", Target = "~M:Logger.#cctor")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsDirectory(System.IO.DirectoryInfo,System.String)~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsAnyDirectory(System.IO.DirectoryInfo,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.DirectoryInfoExtensions.ContainsAllDirectories(System.IO.DirectoryInfo,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsAllDirectories(System.IO.FileInfo,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsDirectory(System.IO.FileInfo,System.String)~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.FileInfoExtensions.ContainsAnyDirectory(System.IO.FileInfo,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.ContainsAny(System.String,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.ContainsAll(System.String,System.String[])~System.Boolean")]
-[assembly: SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>", Scope = "member", Target = "~M:System.StringExtensions.GetLocationsOfString(System.String,System.String)~System.Collections.Generic.List{System.Int32}")]
