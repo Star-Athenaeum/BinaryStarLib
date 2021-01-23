06cb0be3a3a4864fc10992069258b663767d93ff
Connor Shearer <connor_shearer@outlook.com>
2021-01-23 15:01:10 +0000

    Finish off for today

--- Stryxus.Lib.C/sl_json.c
+++ Stryxus.Lib.C/sl_json.c
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "sl_json.h"
+
+bool json_entry_exists(JSON json, string key)
+{
+	return json.find(key) != json.end();
+}
--- Stryxus.Lib.C/sl_json.h
+++ Stryxus.Lib.C/sl_json.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <nlohmann/>
+
+bool json_entry_exists(nlohamm json, string key);
