b7eb01dacdb0ebfa078ce2d38443f4a379d8fa61
Connor Shearer <connor_shearer@outlook.com>
2021-02-07 12:21:29 +0000

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -6,21 +6,20 @@ echo --- Stryxus.Lib.C Dependency Build Tool ---
 echo -----------------------------------
 echo IMPORTANT: This tool MUST be opened in Visual Studio's x64 Native Dev Tools Command Prompt!!!
 echo This process may take several minutes depending on how powerful your CPU is.
-echo Entering the Visual Studio year version will delete the current /solution/build/ folder and begin the build process.
 echo.
 echo.
-set /p id= -- Enter your Visual Studio year version (2019 etc): 
 cd %~dp0
-rd /s /q build
-rd /s /q %~dp0\libsass\win\bin
-mkdir build
-cd build
+rd /s /q Build
+mkdir Build
+cd Build
+mkdir Debug
+cd Debug
 echo.
 echo -- Working at %~dp0
 echo.
 echo -- Running CMakeLists
 echo.
-cmake ../
+cmake ../../
 echo.
 echo.
 echo -- Starting To Build Projects.
@@ -29,7 +28,7 @@ echo.
 echo -------------------- BUILDING cJSON --------------------
 echo.
 echo.
-cmake --build . --target %~dp0build\cJSON\ALL_BUILD
+cmake --build . --target %~dpBuild\Debug\cJSON\ALL_BUILD
 echo.
 echo.
 echo.
