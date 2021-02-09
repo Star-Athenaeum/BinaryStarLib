b57013ea6bd802e2c7053f0fa04eee8e1eba6e31
Connor Shearer <connor_shearer@outlook.com>
2021-02-09 10:55:25 +0000

    Fix a type include issue

--- Stryxus.Lib.C.Shared/sl_libraries.h
+++ Stryxus.Lib.C.Shared/sl_libraries.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #ifdef KERNEL_NT
+#include <Windows.h>
+
 void load_libraries(char** lib_names);
 HMODULE get_library(char* lib_name);
 void* get_lib_function(HMODULE lib, char* func_name);
