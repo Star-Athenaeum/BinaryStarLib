b9f880e49d9ea8c3801f587e50fbe4336098f60b
Connor Shearer <connor_shearer@outlook.com>
2021-02-09 11:03:07 +0000

    Update sl_libraries.h

--- Stryxus.Lib.C.Shared/sl_libraries.h
+++ Stryxus.Lib.C.Shared/sl_libraries.h
@@ -3,14 +3,14 @@
 #ifdef KERNEL_NT
 #include <Windows.h>
 
-void load_libraries(char** lib_names);
+void load_libraries(char* lib_names[]);
 HMODULE get_library(char* lib_name);
 void* get_lib_function(HMODULE lib, char* func_name);
 void free_libraries();
 #endif
 
 #ifdef KERNEL_LINUX
-void load_libraries(char** lib_names);
+void load_libraries(char* lib_names[]);
 void* get_library(char* lib_name);
 void* get_lib_function(void* lib, char* func_name);
 void free_libraries();
